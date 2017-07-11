9fcb67be3ca69c69914e614cc25fbf90b2a4fafd
yugandharc9 <yugandharc9@gmail.com>
2017-07-12 04:16:16 +0530

    button click event connection

--- app/src/main/java/thingslabinternetservicespvtltd/firenotify/MainActivity.java
+++ app/src/main/java/thingslabinternetservicespvtltd/firenotify/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import org.eclipse.paho.android.service.MqttAndroidClient;
 import org.eclipse.paho.client.mqttv3.IMqttActionListener;
@@ -23,92 +25,102 @@ MqttAndroidClient client;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         String clientId = MqttClient.generateClientId();
-        MqttConnectOptions opt=new MqttConnectOptions();
-        opt.setUserName("yugandharc9");
-        System.out.println("123123123");
-        opt.setPassword("Qq1-yugan".toCharArray());
-        opt.setCleanSession(true);
         client =
                 new MqttAndroidClient(this.getApplicationContext(), "tcp://www.alpha.primemq.com:8721",
                         clientId);
 
-        try {
-            IMqttToken token = client.connect(opt);
-            System.out.println("client is "+client.isConnected());
-            System.out.println(token.toString());
-            token.setActionCallback(new IMqttActionListener() {
-                @Override
-                public void onSuccess(IMqttToken asyncActionToken) {
-                    // We are connected
-                    //Log.d("TAG", "onSuccess");
-                    System.out.println("Sucessfully Connected MQTT");
-                    IMqttToken tok = null;
-                    try {
-                        tok = client.subscribe("room/fire",2);
-                        client.setCallback(new MqttCallback() {
-                            @Override
-                            public void connectionLost(Throwable cause) {
+        Button btn=(Button) findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 
-                            }
+                MqttConnectOptions opt=new MqttConnectOptions();
+                opt.setUserName("yugandharc9");
+                System.out.println("123123123");
+                opt.setPassword("Qq1-yugan".toCharArray());
+                opt.setCleanSession(true);
 
-                            @Override
-                            public void messageArrived(String topic, MqttMessage message) throws Exception {
-                                String str=new String(message.getPayload());
-                                System.out.println(str+" is message");
-                            }
-
-                            @Override
-                            public void deliveryComplete(IMqttDeliveryToken token) {
-
-                            }
-                        });
-                    } catch (MqttException e) {
-                        e.printStackTrace();
-                    }
-                    tok.setActionCallback(new IMqttActionListener() {
+                try {
+                    IMqttToken token = client.connect(opt);
+                    System.out.println("client is "+client.isConnected());
+                    System.out.println(token.toString());
+                    token.setActionCallback(new IMqttActionListener() {
                         @Override
                         public void onSuccess(IMqttToken asyncActionToken) {
-                            System.out.println("subscritption suceess");
+                            // We are connected
+                            //Log.d("TAG", "onSuccess");
+                            System.out.println("Sucessfully Connected MQTT");
+                            IMqttToken tok = null;
+                            try {
+                                tok = client.subscribe("room/fire",2);
+                                client.setCallback(new MqttCallback() {
+                                    @Override
+                                    public void connectionLost(Throwable cause) {
+
+                                    }
+
+                                    @Override
+                                    public void messageArrived(String topic, MqttMessage message) throws Exception {
+                                        String str=new String(message.getPayload());
+                                        System.out.println(str+" is message");
+                                    }
+
+                                    @Override
+                                    public void deliveryComplete(IMqttDeliveryToken token) {
+
+                                    }
+                                });
+                            } catch (MqttException e) {
+                                e.printStackTrace();
+                            }
+                            tok.setActionCallback(new IMqttActionListener() {
+                                @Override
+                                public void onSuccess(IMqttToken asyncActionToken) {
+                                    System.out.println("subscritption suceess");
+                                }
+
+                                @Override
+                                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
+
+                                }
+                            });
+
+
                         }
 
                         @Override
                         public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
-
+                            // Something went wrong e.g. connection timeout or firewall problems
+                            //Log.d("TAG", "onFailure");
+                            System.out.println("Not Connected MQTT");
                         }
                     });
-
-
+                } catch (MqttException e) {
+                    System.out.println(e.getMessage().toString());
+                    e.printStackTrace();
                 }
+                client.setCallback(new MqttCallback() {
+                    @Override
+                    public void connectionLost(Throwable cause) {
 
-                @Override
-                public void onFailure(IMqttToken asyncActionToken, Throwable exception) {
-                    // Something went wrong e.g. connection timeout or firewall problems
-                    //Log.d("TAG", "onFailure");
-                    System.out.println("Not Connected MQTT");
-                }
-            });
-        } catch (MqttException e) {
-            System.out.println(e.getMessage().toString());
-            e.printStackTrace();
-        }
-        client.setCallback(new MqttCallback() {
-            @Override
-            public void connectionLost(Throwable cause) {
+                    }
+
+                    @Override
+                    public void messageArrived(String topic, MqttMessage message) throws Exception {
+                        System.out.println(message.getPayload().toString() + " is message");
+                    }
+
+                    @Override
+                    public void deliveryComplete(IMqttDeliveryToken token) {
+
+                    }
+
+
+
+                });
+
 
             }
-
-            @Override
-            public void messageArrived(String topic, MqttMessage message) throws Exception {
-                System.out.println(message.getPayload().toString() + " is message");
-            }
-
-            @Override
-            public void deliveryComplete(IMqttDeliveryToken token) {
-
-            }
-
-
-
         });
 
     }
