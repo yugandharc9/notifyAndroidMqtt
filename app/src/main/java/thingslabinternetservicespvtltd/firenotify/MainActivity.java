de49f939781a045aa08fd60942d3c15de599c92f
yugandharc9 <yugandharc9@gmail.com>
2017-07-12 15:58:53 +0530

    Notification & MQTT connection on button click

--- app/src/main/java/thingslabinternetservicespvtltd/firenotify/MainActivity.java
+++ app/src/main/java/thingslabinternetservicespvtltd/firenotify/MainActivity.java
@@ -1,9 +1,13 @@
 package thingslabinternetservicespvtltd.firenotify;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.provider.Settings;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -20,6 +24,7 @@ import org.eclipse.paho.client.mqttv3.MqttMessage;
 
 public class MainActivity extends AppCompatActivity {
 MqttAndroidClient client;
+    NotificationCompat.Builder builder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,16 +33,15 @@ MqttAndroidClient client;
         client =
                 new MqttAndroidClient(this.getApplicationContext(), "tcp://www.alpha.primemq.com:8721",
                         clientId);
-
+        builder=new NotificationCompat.Builder(this);
         Button btn=(Button) findViewById(R.id.button);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 MqttConnectOptions opt=new MqttConnectOptions();
-                opt.setUserName("USERNAME");
+                opt.setUserName("yugandharc9");
                 System.out.println("123123123");
-                opt.setPassword("PASSWORD".toCharArray());
+                opt.setPassword("Qq1-yugan".toCharArray());
                 opt.setCleanSession(true);
 
                 try {
@@ -63,6 +67,19 @@ MqttAndroidClient client;
                                     public void messageArrived(String topic, MqttMessage message) throws Exception {
                                         String str=new String(message.getPayload());
                                         System.out.println(str+" is message");
+                                        Integer in=new Integer(str);
+                                      if (1==in){
+                                          System.out.println("yes its running");
+                                            builder.setSmallIcon(R.drawable.if_flame);
+                                            builder.setContentTitle("Fire Alert");
+                                            builder.setContentText("Fire has been detected on your property. Please evacuate the place immediately." +
+                                                    "We are sending help.");
+                                            builder.setPriority(2);
+                                            builder.setAutoCancel(false);
+                                            Notification n = builder.build();
+                                            NotificationManager mgr = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+                                            mgr.notify(2154, n);
+                                        }
                                     }
 
                                     @Override
